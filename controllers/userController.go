1a8f7d4b9e03037d4b700a7c74d20a08e755facf
Emmanuel Ogbiyoyo <ogbiyoyosky@yahoo.com>
2023-01-06 15:27:47 +0000

    Add log

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,13 +1,15 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	connection "github.com/ogbiyoyosky/travex/db"
 	"github.com/ogbiyoyosky/travex/models"
 )
 
 func GetProfile(c *fiber.Ctx) error {
-
+	fmt.Println("Called profile")
 	var user models.User
 	userObj := c.Locals("user").(models.User)
 
